7f416571add6e579c039b49edb8d4ef0baf02dce
Preston Zhang <preston0769@gmail.com>
2018-10-16 20:05:30 +1100

    Remove old files

--- lib/actions/app_actions.dart
+++
@@ -1,6 +0,0 @@
-class newChatMsgReceivedAction{
-   final int idFrom;
-   final int idTo;
-   final String content;
-   newChatMsgReceivedAction(this.idFrom,this.idTo,this.content);
-}
--- lib/keys/global_keys.dart
+++
@@ -1,5 +0,0 @@
-
-import 'package:flutter/material.dart';
-
-final chatScreenKey = new GlobalKey(debugLabel: "GlobalChatScreen");
-final mainAppKey= new GlobalKey(debugLabel: "GlobalMainApp");
